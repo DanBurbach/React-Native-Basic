4224015a659e7ca560df0dc90d516304044663d2
DanBurbach <dburbach1982@gmail.com>
2019-12-01 22:09:46 -0800

    generate new pseudo CSS for Text and View parts of styles in App main page

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>This is a header!!</Text>
+      </View>
+      <View style={styles.bodyText}>
+        <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </Text>
+      </View>
     </View>
   );
 }
@@ -16,4 +21,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'lightgreen',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  bodyText: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  }
 });
