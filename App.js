52cfac244374460160680859476ba4eed23c40b7
DanBurbach <dburbach1982@gmail.com>
2019-12-02 20:47:04 -0800

    generate React hook for display of name, place button for future update of name below

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Danno');
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -9,6 +11,11 @@ export default function App() {
       </View>
       <View style={styles.bodyText}>
         <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </Text>
+      </View>
+      <Text>My name is (written with a React hook)... {name} </Text>
+      <Text></Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update name here'/>
       </View>
     </View>
   );
