eb71546fcbe8d7fe100df0b83fe68d925bc8b4b4
DanBurbach <dburbach1982@gmail.com>
2019-12-02 22:39:01 -0800

    render FlatList items from setPeople Hook. Use ids, people, and name for rendering

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Danno');
+  const [name, seName] = useState('Danno');
 
   const [age, setAge] = useState('37')
+
+const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
+
   //passes object in
   // const [anyname, setAnyname] = useState({ name: 'Danno', age: 37});
 
@@ -48,6 +59,15 @@ export default function App() {
         <Button title='update name here' onPress={clickName}/>
       </View> */}
 
+      <FlatList 
+        numColumns={1}
+        keyExtractor={(item) => item.id} 
+        data={people} 
+        renderItem={({ item }) => ( 
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
     </View>
   );
 }
@@ -58,8 +78,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20,
   },
   header: {
+    marginTop: 50,
     backgroundColor: 'lightgreen',
     padding: 20,
   },
@@ -76,5 +98,14 @@ const styles = StyleSheet.create({
     padding: 4,
     margin: 10,
     width: 200,
-  }
+  },
+  item: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 10,
+    padding: 80,
+    backgroundColor: 'lightblue',
+    fontSize: 18,
+    textAlign: "center",
+  },
 });
