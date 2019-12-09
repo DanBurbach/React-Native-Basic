27dd5da1e4e13fbd326ce9a4bc309e6828bfa0f1
DanBurbach <dburbach1982@gmail.com>
2019-12-09 10:05:59 -0800

    add stylesheet data for Flexbox

--- components/Sandbox.js
+++ components/Sandbox.js
@@ -3,10 +3,41 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Sandbox() {
     return (
-        <Text>sandbox tester</Text>
+        <View style={styles.container}>
+            <Text styles={styles.boxOne}> One </Text>
+            <Text styles={styles.boxTwo}> Two </Text>
+            <Text styles={styles.boxThree}> Three </Text>
+            <Text styles={styles.boxFour}> Four </Text>
+            <Text styles={styles.boxFive}> Five </Text>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        // flex takes up whole page at setting of 1
+        // flex: 1,
+        paddingTop: 40,
+        // backgroundColor: '#ddd',
+    },
+    boxOne: {
+        backgroundColor: 'rgb(43,12,8)',
+        padding: 10,
+    },
+    boxTwo: {
+        backgroundColor: 'gold',
+        padding: 10,
+    },
+    boxThree: {
+        backgroundColor: 'pink',
+        padding: 10,
+    },
+    boxFour: {
+        backgroundColor: 'yellow',
+        padding: 10,
+    },
+    boxFive: {
+        backgroundColor: 'violet',
+        padding: 10,
+    }
 });
