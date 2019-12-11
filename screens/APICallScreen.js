f2b7390a62a6a05fcf621318875b29f8f9b1c847
DanBurbach <dburbach1982@gmail.com>
2019-12-10 23:27:33 -0800

    using an outside API call, while checking state and for errors, pass data into a map from an API call to be displayed in list format

--- screens/APICallScreen.js
+++ screens/APICallScreen.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import { StyleSheet, Button, ScrollView, FlatList, Alert, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 
-export default class FlexboxScreen extends React.Component{
+export default class App extends React.Component {
     constructor(){
         super();
         this.state = {
             data: null,
-            loadedPage: true,
+            loaded: true,
             error: null
         }
     }
-
     coreURL = 'https://jsonplaceholder.typicode.com';
 
     getData = (ev) => {
-        let url = this.coreUrl + '/comments';
-        let headers = new Headers();
-
+        this.setState({loaded:false, error: null});
+        let url = this.coreURL + '/comments';
+        let h = new Headers();
+        h.append('Authorization', 'Writer asdfjklqwerty');
+        h.append('X-Client', 'Danno');
+        
         let req = new Request(url, {
+            headers: h,
             method: 'GET'
         });
-
+        
         fetch(req)
         .then(response=>response.json())
         .then(this.showData)
-        .catch(this.badData)
+        .catch(this.badStuff)
     }
 
     showData = (data) => {
@@ -37,25 +40,45 @@ export default class FlexboxScreen extends React.Component{
 
     render() {
         return (
-            <ScrollView>
-                <Button 
-                    color='orange' 
+            <View>
+                <View style={styles.container}>{ !this.state.loaded && (<Text>Loading...</Text>)}
+                </View>
+                <Button
+                    color= 'orange'
+                    style={styles.button}
                     onPress={this.getData} 
                     title='Grab API Data' />
-            </ScrollView>
+                <ScrollView>
+                    { this.state.error !== null && (
+                        <Text style={styles.errors}>{this.state.error}</Text>
+                    )}
+                    { this.state.data && this.state.data.length > 0 && (
+                        this.state.data.map( comment => (
+                            <Text key={comment.id} style={styles.txt}>
+                            { comment.name }    
+                            </Text>
+                        ))
+                    )}
+                </ScrollView>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        // flex takes up whole page at setting of 1
         flex: 1,
-        backgroundColor: '#ddd',
-        // everything inside the flex container becomes a flex item, default column
-        flexDirection: 'row',
-        justifyContent: 'space-around',
+        backgroundColor: '#fff',
         alignItems: 'center',
-        height: '100%',
+        justifyContent: 'center',
+    },
+    button: {
+        color: 'orange',
+        backgroundColor: 'orange',
+    },
+    error: {
+        color: 'red',
+        fontSize: 30,
+        fontWeight: 'bold'
     }
 })
