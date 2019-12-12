c3f6bf20bac9733ca290db4a715c497fc0ac923d
DanBurbach <dburbach1982@gmail.com>
2019-12-12 14:10:26 -0800

    using React-Native provided icons in expo, generate an image next to the Todo Item as to inform the user that when clicked it will send to trash

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import  { MaterialIcons } from '@expo/vector-icons';
+
 
 export default function TodoItem ({ item, pressHandler }) {
     return (
         <TouchableOpacity onPress={()=> pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={18} color='#333'/>
+            <Text style={styles.itemText}>{item.text}</Text>
+            </View>
         </TouchableOpacity> 
     )
 }
@@ -16,6 +21,10 @@ const styles = StyleSheet.create({
         borderColor: 'lightgrey',
         borderWidth: 1,
         borderStyle: 'dashed',
-        borderRadius: 10
+        borderRadius: 10,
+        flexDirection: 'row',
+    },
+    itemText: {
+        marginLeft: 10,
     }
 })
